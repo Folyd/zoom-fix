4b5ee614ca8c826022159590863b213d78ee8034
Folyd <lyshuhow@gmail.com>
2019-09-09 16:09:35 +0800

    Fix windows build, release on master branch

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -1,6 +1,3 @@
 cargo build --release
-mkdir staging
-copy target\release\%PROJECT_NAME%.exe staging
-copy target\release\build\*\out\_%PROJECT_NAME%.ps1 staging
-cd staging
-7z a ../%PROJECT_NAME%-%TRAVIS_TAG%-%TARGET%.zip *
+Get-ChildItem
+7z a %PROJECT_NAME%-%TRAVIS_TAG%-%TARGET%.zip target\release\%PROJECT_NAME%.exe
