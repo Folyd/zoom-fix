3ca496173fee0b033a41e18c22e09ea0cb955725
Folyd <lyshuhow@gmail.com>
2019-09-09 18:32:50 +0800

    Check the hosts file contains head line to prevent append one more times

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 use std::fs::OpenOptions;
-use std::io::Write;
+use std::io::{Read, Write};
 
 fn main() {
+    let path = if cfg!(target_os = "windows") {
+        "C:\\Windows\\System32\\drivers\\etc\\hosts"
+    } else {
+        "/etc/hosts"
+    };
+
     let hosts = include_str!("hosts");
     println!("{}", hosts);
 
-    if cfg!(target_os = "windows") {
-        // C:\Windows\System32\drivers\etc\
-        match OpenOptions::new().append(true).open("C:\\Windows\\System32\\drivers\\etc\\hosts") {
-            Ok(mut file) => {
+    match OpenOptions::new().read(true).append(true).open(path) {
+        Ok(mut file) => {
+            let mut content = String::new();
+            file.read_to_string(&mut content).unwrap();
+            if !content.contains("# ZOOM IP list") {
                 file.write_all(hosts.as_bytes()).unwrap();
             }
-            Err(err) => { println!("err: {}", err) }
         }
-    } else {
-        // /etc/hosts
-        match OpenOptions::new().append(true).open("/etc/hosts") {
-            Ok(mut file) => {
-                file.write_all(hosts.as_bytes()).unwrap();
-            }
-            Err(err) => { println!("err: {}", err) }
-        }
+        Err(err) => { println!("err: {}", err) }
     }
 }
