a83c8385540594355476ff7a96bc50e399c69125
Folyd <lyshuhow@gmail.com>
2019-09-09 16:54:27 +0800

    Fix build error

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -1,3 +1,3 @@
-cargo build --release
-Get-ChildItem target\release
-7z a $Env:PROJECT_NAME-$Env:TRAVIS_TAG-Env:TARGET.zip target\release\$ENV:PROJECT_NAME.exe
+Get-ChildItem target\$Env:TARGET\release
+7z a $Env:PROJECT_NAME-$Env:TRAVIS_TAG-$Env:TARGET.zip target\$Env:TARGET\release\$Env:PROJECT_NAME.exe
+Get-ChildItem
